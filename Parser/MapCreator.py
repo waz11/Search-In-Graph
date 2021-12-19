a21d5d192add6eb4827a605e1b9efe1f3f989dbb
waz11 <ronisr@post.bgu.ac.il>
2021-12-19 09:35:59 +0200

    graph building

--- Parser/MapCreator.py
+++ Parser/MapCreator.py
@@ -117,7 +117,7 @@ class MapCreator:
         query_key = key
 
         handle_task(mapped_task_dict, code.query, key, comments=None, tags=code.tags,
-                    score=code.score, url=code.url, type="Folder", post=code.text)
+                    score=code.score, url=code.url, type="project", post=code.text)
         key = key + 1
         """append the task to the map"""
         full_task_dict["vertices"].append(mapped_task_dict)
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from Parser.CodeFromFile import CodeFromFile
 
 
-def main():
-    file_path = 'Files/src'
-    output_path = 'Files/out.json'
-
-    code_from_file = CodeFromFile(file_path,'', output_path)
+def code_to_graph(project_path, project_name, output_path):
+    code_from_file = CodeFromFile(project_path,project_name, output_path)
     code_from_file.concat_files()
     code_from_file.test_new_file()
+
+def main():
+    code_to_graph('Files/src','Iterator_Practice', 'Files/out.json')
 
 
 if __name__ == '__main__':
