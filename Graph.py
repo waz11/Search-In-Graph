96827027cea77ef3986b74583c17b678ecb18d8b
waz11 <ronisr@post.bgu.ac.il>
2022-01-01 17:34:47 +0200

    json go graph

---
+++ Graph.py
@@ -0,0 +1,68 @@
+import json
+
+
+class Graph:
+
+    def __init__(self, path):
+        self.graph = {}
+        self.vertex_info = {}
+        self.vertex_type = {}
+        self.edge_info = {}
+        self.__build_graph(path)
+
+    def __build_graph(self, path):
+        f = open(path)
+        data = json.load(f)
+        for node in data['vertices']:
+            self.graph[node["key"]] = []
+            self.vertex_info[node["key"]] = node["name"]
+            self.vertex_type[node["key"]] = node["type"]
+        for edge in data['edges']:
+            self.graph[edge["from"]].append(edge["to"])
+            self.edge_info[(edge["from"], edge["to"])] = edge["type"]
+        f.close()
+
+        print(self.graph)
+        print(self.vertex_info)
+        print(self.vertex_type)
+        print(self.edge_info)
+
+    def num_of_vertices(self):
+        return len(self.vertex_info)
+
+    def neighbors(self, key):
+        return self.graph[key]
+
+    def arc_type(self, from_node, to_node):
+        return self.edge_info[(from_node, to_node)]
+
+    def vertex_text(self, key):
+        return self.vertex_info[key]
+
+    def get_first_node(self):
+        return self.vertex_info[-1]
+
+    def get_type(self, id):
+        return self.vertex_type[id]
+
+    def bfs(self):
+        print(str(self.vertex_info))
+        print(str(self.graph))
+        queue = [-1]
+        visited = {}
+        for key in self.graph:
+            visited[key] = False
+        while queue:
+            vertex = queue.pop(0)
+            visited[vertex] = True
+            print("ID:" + str(vertex) + " Text: " + self.vertex_info[vertex])
+            print("My neighbor:")
+            i = 0
+            for key in self.graph[vertex]:
+                count = count + len(key.split())
+                i += 1
+            #     if not visited[key]:
+            #         queue.append(key)
+            #     print("ID:"+str(key)+" Text: "+self.vertex_info[key])
+            # print("--------------------------------------------------")
+        print("AVG " + count / i)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-
 from Parser.CodeFromFile import CodeFromFile
+from Graph import Graph
 
 
 def code_to_graph(project_path, output_path):
@@ -11,9 +11,12 @@ def code_to_graph(project_path, output_path):
 
 
 def main():
-    code_to_graph('Files/src', 'Files/out.json')
-    code_to_graph('Files/src2','Files/out2.json')
-    code_to_graph('Files/src3', 'Files/out3.json')
+    # code_to_graph('Files/src', 'Files/out.json')
+    # code_to_graph('Files/src2','Files/out2.json')
+    # code_to_graph('Files/src3', 'Files/out3.json')
+
+    g = Graph('Files/out.json')
+
 
 if __name__ == '__main__':
     main()
