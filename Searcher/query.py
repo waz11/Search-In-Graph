6cae9f5efb2bc52ba72eb76ccae6d2a0a92726e9
waz11 <ronisr@post.bgu.ac.il>
2022-01-03 20:15:09 +0200

    query parser

--- Searcher/query.py
+++ Searcher/query.py
@@ -1,4 +1,105 @@
+import string
+
 class Query:
 
-    def __init__(self):
-        pass
+    def __init__(self, query):
+        self.content = query.split(',')
+        self.key = -1
+        self.classes_vertex_map = {}
+        self.methods_vertex_map = {}
+        self.edges_map = {}
+        self.special_words = set(["extends", "implements", "contains", "method","class","gets"])
+        self.graph = {}
+
+    def get_uniqe_key(self) -> int:
+        self.key+=1
+        return self.key
+
+    def parse(self):
+        q = self.content
+        for sentence in q:
+            print(sentence)
+            words = list(sentence.split(' '))
+            for i, word in enumerate(words):
+                if word in self.special_words:
+                    if word=='extends':
+                        vertex1 = self.create_vertex(words[i-1],"class")
+                        vertex2 = self.create_vertex(words[i+1],"class")
+                        self.create_edge("extends", vertex2["name"], vertex1["name"])
+                    elif word=='implements':
+                        vertex1 = self.create_vertex(words[i-1],"class")
+                        vertex2 = self.create_vertex(words[i+1],"class")
+                        self.create_edge("implements", vertex2["name"], vertex1["name"])
+                    # elif word=='gets':
+                    #     vertex1 = self.create_vertex(words[i-1],"method")
+                    #     vertex1["attributes"].append(words[i+1])
+
+
+    def create_vertex(self, name:string, type:string, attributes:list=[]):
+        if type == 'class':
+            if name not in self.classes_vertex_map.keys():
+                vertex = {}
+                vertex["name"] = name
+                vertex["key"] = self.get_uniqe_key()
+                vertex["type"] = type
+                if len(attributes) > 0:
+                    vertex["attributes"]
+                self.classes_vertex_map[name] = vertex
+            return self.classes_vertex_map[name]
+        if type == 'method':
+            if name not in self.methods_vertex_map.keys():
+                vertex = {}
+                vertex["name"] = name
+                vertex["key"] = self.get_uniqe_key()
+                vertex["type"] = type
+                if len(attributes) > 0:
+                    vertex["attributes"]
+                self.methods_vertex_map[name] = vertex
+            return self.methods_vertex_map[name]
+
+
+
+    def create_edge(self, type, source, to):
+        edge = {}
+        edge["type"] = type
+        if source not in self.classes_vertex_map.keys():
+            self.create_vertex(source)
+        edge["from"] = source
+        edge["to"] = to
+        self.edges_map[source,to] = edge
+
+    def build(self):
+        list1 = list(self.classes_vertex_map.values())
+        list2 = list(self.methods_vertex_map.values())
+        vertices = list1+list2
+        self.graph["vertices"] = vertices
+
+        self.graph["edges"] = list(self.edges_map.values())
+        print(self.graph)
+
+
+def main():
+    # query = "c1 extends c2,
+    # c3 implements c4,
+    # m1 gets f1 and f2 and f3,
+    # c5 with m2 and m3 and m4,
+    # c6 contains c7"
+    query = "c3 implements c4"
+    q = Query(query)
+    q.parse()
+    q.build()
+
+
+
+
+    # key = 0
+    # for i,v in enumerate(q):
+    #     if v == "extends":
+    #         create_vertex(name=q[i-1], key=key, type="class")
+    #         key+=1
+    #         create_vertex(name=q[i+1], key=key, type="class")
+    #         key+=1
+
+
+if __name__ == '__main__':
+    main()
