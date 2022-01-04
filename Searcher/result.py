8d22dded2cffdac14e79f6f7297d455dab9307b5
waz11 <ronisr@post.bgu.ac.il>
2022-01-05 01:03:42 +0200

    result object

--- Searcher/result.py
+++ Searcher/result.py
@@ -1,4 +1,12 @@
+from Graph.graph import Graph
+
+
 class Result:
 
-    def __init__(self):
-        pass
+    def __init__(self, graph:Graph, rank=0.0):
+        self.graph = graph
+        self.rank = rank
+
+    def inc_rank(self, n):
+        self.rank += n
+
