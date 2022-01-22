d99ac2f76d1824d8097d536da1fa40319b7e62ab
Ron <ronisr@post.bgu.ac.il>
2022-01-23 01:09:15 +0200

    adding queries

--- Parser/codeToGraph/code_to_graph.py
+++ Parser/codeToGraph/code_to_graph.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import javalang
 from Graph.graph import Graph
 from javalang.parse import parse
-from Parser.codeToGraph.handlers import class_handler
+from Parser.codeToGraph.handlers import foo
 from Parser.codeToGraph.types import typeof
 
 primitive_variables = set(['String','boolean', 'byte', 'char', 'double','float', 'int', 'long', 'short'])
@@ -33,54 +33,58 @@ class CodeParser:
                 pass
         return result
 
+
+    # def foo(self, graph, x):
+    #     class_component = class_handler(x)
+    #     name = class_component.name
+    #     if name.lower() == 'main':
+    #         return
+    #
+    #     type = class_component.type
+    #     modifiers = class_component.modifiers
+    #     class_vertex = self.graph.add_class(name, modifiers)
+    #
+    #     for attr in class_component.fields:
+    #         if attr.type not in unwanted_types:
+    #             class_vertex.add_attribute(attr.type)
+    #             field_type_vertex = self.graph.add_class(attr.type, 'class')
+    #             self.graph.add_edge('field', class_vertex, field_type_vertex)
+    #
+    #     for method in class_component.methods:
+    #         name = method.name
+    #         if name.lower == 'main':
+    #             continue
+    #         arguments = []
+    #         for arg in method.arguments:
+    #             if arg not in unwanted_types:
+    #                 arguments.append(arg)
+    #         return_type = method.return_type
+    #         method_vertex = self.graph.add_method(name, arguments, modifiers, return_type)
+    #         self.graph.add_edge('method', class_vertex, method_vertex)
+    #
+    #         for arg in arguments:
+    #             arg_vertex = self.graph.add_class(arg)
+    #             self.graph.add_edge('argument', method_vertex, arg_vertex)
+    #
+    #     for inner_class in class_component.inner_classes:
+    #         name = inner_class.name
+    #
+    #     if class_component.extends:
+    #         name = class_component.extends
+    #         extended_class_vertex = self.graph.add_class(name, 'class')
+    #         self.graph.add_edge('extends', class_vertex, extended_class_vertex)
+    #
+    #     for interface_name in class_component.implements:
+    #         interface_vertex = self.graph.add_interface(interface_name)
+    #         self.graph.add_edge('implements', class_vertex, interface_vertex)
+
+
     def build_graph(self, graph:Graph) ->None:
         code = self.concat_files(self.directory_path)
         parsed_code = parse(code)
         for x in parsed_code.types:
             if (typeof(x) == 'class' or typeof(x) == 'interface'):
-                class_component = class_handler(x)
-                name = class_component.name
-                if name.lower() == 'main':
-                    continue
-
-                # type = class_component.type
-                modifiers = class_component.modifiers
-                class_vertex = graph.add_class(name, modifiers)
-
-                for attr in class_component.fields:
-                    if attr.type not in unwanted_types:
-                        class_vertex.add_attribute(attr.type)
-                        field_type_vertex = graph.add_class(attr.type, 'class')
-                        graph.add_edge('field', class_vertex, field_type_vertex)
-
-                for method in class_component.methods:
-                    name = method.name
-                    if name.lower == 'main':
-                        continue
-                    arguments = []
-                    for arg in method.arguments:
-                        if arg not in unwanted_types:
-                            arguments.append(arg)
-                    return_type = method.return_type
-                    method_vertex = graph.add_method(name, arguments, modifiers, return_type)
-                    graph.add_edge('method', class_vertex, method_vertex)
-
-                    for arg in arguments:
-                        arg_vertex = graph.add_class(arg)
-                        graph.add_edge('argument', method_vertex, arg_vertex)
-
-                for inner_class in class_component.inner_classes:
-                    name = inner_class.name
-
-
-                if class_component.extends:
-                    name = class_component.extends
-                    extended_class_vertex = graph.add_class(name, 'class')
-                    graph.add_edge('extends', class_vertex, extended_class_vertex)
-
-                for interface_name in class_component.implements:
-                    interface_vertex = graph.add_interface(interface_name)
-                    graph.add_edge('implements', class_vertex, interface_vertex)
+                foo(self.graph, x)
             # else:
             #     print(x)
 
@@ -97,7 +101,7 @@ def main():
     # g = CodeParser('../../Files/codes/lucene-master/demo/src/main/java').graph
     # g.save_to_json_file('../../Files/json graphs/lucene-master.json')
 
-    g = CodeParser('../../Files/codes/poi').graph
+    g = CodeParser('../../Files/codes/ron').graph
     g.save_to_json_file('../../Files/json graphs/poi.json')
 
 if __name__ == '__main__':
--- Parser/codeToGraph/handlers.py
+++ Parser/codeToGraph/handlers.py
@@ -9,6 +9,59 @@ from Parser.codeToGraph.components.fieldComponent import FieldComponent
 from Parser.codeToGraph.components.methodComponent import MethodComponent
 from Parser.codeToGraph.types import typeof
 
+primitive_variables = set(['String','boolean', 'byte', 'char', 'double','float', 'int', 'long', 'short'])
+generic_types = set(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+unwanted_types = primitive_variables.union(generic_types)
+
+
+def foo(graph, x):
+    class_component = class_handler(x)
+    name = class_component.name
+    if name.lower() == 'main':
+        return
+
+    type = class_component.type
+    modifiers = class_component.modifiers
+    class_vertex = graph.add_class(name, modifiers)
+
+    for attr in class_component.fields:
+        if attr.type not in unwanted_types:
+            class_vertex.add_attribute(attr.type)
+            field_type_vertex = graph.add_class(attr.type, 'class')
+            graph.add_edge('field', class_vertex, field_type_vertex)
+
+    for method in class_component.methods:
+        name = method.name
+        if name.lower == 'main':
+            continue
+        arguments = []
+        for arg in method.arguments:
+            if arg not in unwanted_types:
+                arguments.append(arg)
+        return_type = method.return_type
+        method_vertex = graph.add_method(name, arguments, modifiers, return_type)
+        graph.add_edge('method', class_vertex, method_vertex)
+
+        for arg in arguments:
+            arg_vertex = graph.add_class(arg)
+            graph.add_edge('argument', method_vertex, arg_vertex)
+
+    for inner_class in class_component.inner_classes:
+        inner_class_vertex = foo(graph, inner_class)
+        graph.add_edge('inner class', class_vertex, inner_class_vertex)
+
+
+    if class_component.extends:
+        name = class_component.extends
+        extended_class_vertex = graph.add_class(name, 'class')
+        graph.add_edge('extends', class_vertex, extended_class_vertex)
+
+    for interface_name in class_component.implements:
+        interface_vertex = graph.add_interface(interface_name)
+        graph.add_edge('implements', class_vertex, interface_vertex)
+
+    return class_vertex
+
 
 def class_handler(component)->ClassComponent:
     name = component.name
@@ -33,18 +86,21 @@ def class_handler(component)->ClassComponent:
         modifier.add('interface')
 
     class_comp = ClassComponent(name, modifier, extends, implements_list,inner_classes, interface)
-    for comp in component.body:
-        if typeof(comp) == 'class':
-            inner_class_comp = class_handler(comp)
-            inner_classes.append(inner_class_comp)
+    try:
+        for comp in component.body:
+            if typeof(comp) == 'class':
+                inner_class_comp = class_handler(comp)
+                inner_classes.append(inner_class_comp)
 
-        elif (typeof(comp) == 'field'):
-            field = field_handler(comp)
-            class_comp.fields.append(field)
+            elif (typeof(comp) == 'field'):
+                field = field_handler(comp)
+                class_comp.fields.append(field)
 
-        elif (typeof(comp) == 'method' and comp.name.lower() != 'main'):
-            method = method_handler(comp)
-            class_comp.methods.append(method)
+            elif (typeof(comp) == 'method' and comp.name.lower() != 'main'):
+                method = method_handler(comp)
+                class_comp.methods.append(method)
+    except: None
+    print(class_comp)
     return class_comp
 
 def field_handler(component)->FieldComponent:
